4b6133cc5159942b181fb3ae821d46776977d09d
Chris Yoo <woyoo@umich.edu>
2023-10-17 18:15:53 -0400

    create navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { Home } from "./Home";
 import { Profile } from "./Profile";
+import { Nav } from "./Nav";
 
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
---
+++ client/src/Nav.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export const Nav = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/profile">Profile</Link>
+      </li>
+    </ul>
+  );
+};
