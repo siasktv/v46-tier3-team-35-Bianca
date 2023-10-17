600eeece943c7cd6217eded94dd971a86644ba9c
Amber Hunt <amberhunt955@gmail.com>
2023-10-16 22:22:40 -0400

    initialized express app

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express();
+
+// test route
+app.get('/', function(req, res) {
+    res.send('<h1>Hello, world!</h1>')
+})
+
+// listen at the designated port
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}...`);
+})
+
