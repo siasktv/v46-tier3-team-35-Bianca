c314acdbb43f62f6d311b5c15b32548f24907e6e
siasktv <biankmbm92@gmail.com>
2023-10-18 19:26:10 -0400

    Create/Crypto/Route

---
+++ server/routes/cryptoRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { createCryptoData } = require("../controllers/cryptoController");
+
+router.post("/create", createCryptoData);
+
+module.exports = router;
