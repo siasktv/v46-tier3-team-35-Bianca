800c99904dabae51d3a3c023e8de65ab6bb3051b
Amber Hunt <amberhunt955@gmail.com>
2023-10-25 16:29:59 -0400

    added create user route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,22 @@
 const User = require("../models/user");
 
+// test route
 const test = (req, res) => {
   res.status(200).json({ message: "test successfully." });
 };
 
+// create user
+const createUser = async (req, res) => {
+  console.log('[From POST handler]', req.body)
+  try {
+    const user = await User.create(req.body);
+    console.log(user);
+  } catch (error) {
+    res.status(400).json(error)
+    console.log(error);
+  }
+}
+
 module.exports = {
-  test,
+  test, createUser
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { test } = require("../controllers/userController");
+const { test, createUser } = require("../controllers/userController");
 
+// test route
 router.get("/test", test);
+
+// create a new user
+router.post("/create", createUser)
 
 module.exports = router;
