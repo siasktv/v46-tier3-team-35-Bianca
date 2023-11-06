a7ff64f2d7aec5c1a5325e6a9e29907a0b64e4de
Jamal Kayed <119996570+Yuroq@users.noreply.github.com>
2023-11-06 00:18:34 -0500

    Update CryptoList.jsx

--- client/src/Components/CryptoList.jsx
+++ client/src/Components/CryptoList.jsx
@@ -1,41 +1,33 @@
 import { useState, useEffect } from "react";
-
-function CryptoList() {
-  const [cryptos, setCryptos] = useState([]);
+import { AiOutlineCaretDown } from "react-icons/ai";
+import { AiOutlineCaretUp } from "react-icons/ai";
+function CategoriesList() {
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
-    )
+    fetch("https://api.coingecko.com/api/v3/coins/categories")
       .then((response) => response.json())
-      .then((data) => setCryptos(data))
+      .then((data) => setCategories(data))
       .catch((error) => console.error(error));
   }, []);
-
   return (
     <div className="overflow-x-auto">
       <table className="table" style={{ width: "1400px" }}>
         <thead>
           <tr>
             <th>Name</th>
-            <th>Price</th>
-            <th>Cap</th>
-            <th>24hr high</th>
+            <th>Market Cap</th>
+            <th>Top Gainer</th>
+            <th>Volume</th>
+            <th>23hr volume</th>
+            <th>Top 3</th>
           </tr>
         </thead>
         <tbody>
-          {cryptos.map((crypto, index) => {
+          {categories.map((crypto, index) => {
             return (
               <tr key={index}>
                 <td>
                   <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle w-12 h-12">
-                        <img
-                          src={crypto.image}
-                          alt="Avatar Tailwind CSS Component"
-                        />
-                      </div>
-                    </div>
                     <div>
                       <div className="font-bold">{crypto.id}</div>
                       <div className="text-sm opacity-50">{crypto.symbol}</div>
@@ -43,24 +35,65 @@ function CryptoList() {
                   </div>
                 </td>
                 <td>
-                  {crypto.current_price
-                    ? crypto.current_price
-                        .toString()
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                    : "undefined"}
+                  ${crypto.market_cap ? crypto.market_cap.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","): 'undefined'}
                   <br />
-                  {/* <span className="badge badge-ghost badge-sm">Desktop Support Technician</span> */}
                 </td>
                 <td>
-                  {crypto.market_cap
-                    ? crypto.market_cap
-                        .toString()
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                    : "undefined"}
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={crypto.top_3_coins[0]}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
                   <br />
-                  {/* <span className="badge badge-ghost badge-sm">Desktop Support Technician</span> */}
                 </td>
-                <td>${crypto.high_24h}</td>
+                <td>
+                  ${crypto.volume_24h ? crypto.volume_24h : 'undefined'}
+                  <br />
+                </td>
+                <td>
+                  {" "}
+                  {crypto.market_cap_change_24h ? +crypto.market_cap_change_24h < 0 ? (
+                    <p style={{ display: "flex" }}>
+                      <AiOutlineCaretDown style={{ color: "red" }} />$
+                      {crypto.market_cap_change_24h}
+                    </p>
+                  ) : (
+                    <p style={{ display: "flex" }}>
+                      <AiOutlineCaretUp style={{ color: "green" }} /> $
+                      {crypto.market_cap_change_24h}
+                    </p>
+                  ) : 'undefined'}
+                </td>
+                <td>
+                  <div className="avatar" style={{ paddingRight: 10 }}>
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={crypto.top_3_coins[0]}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div className="avatar" style={{ paddingRight: 10 }}>
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={crypto.top_3_coins[1]}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={crypto.top_3_coins[2]}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <br />
+                </td>
                 <th>
                   <button
                     className="btn btn-ghost btn-xs"
@@ -73,17 +106,9 @@ function CryptoList() {
             );
           })}
         </tbody>
-        <tfoot>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Rank</th>
-            <th></th>
-          </tr>
-        </tfoot>
       </table>
     </div>
   );
 }
 
-export default CryptoList;
+export default CategoriesList;
