79812846e3e78d76aa4dc177c67897e4eb55a365
siasktv <biankmbm92@gmail.com>
2023-11-13 16:50:00 -0500

    Resolved merge conflicts in cryptoRoutes.js

--- server/routes/cryptoRoutes.js
+++ server/routes/cryptoRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const { createCryptoData, getCryptoData, getCryptoDataById } = require("../controllers/cryptoController");
 
 router.post("/create", createCryptoData);
-<<<<<<< HEAD
-=======
 router.get("/", getCryptoData );
 router.get("/:id", getCryptoDataById);
->>>>>>> 079c8786787b9429f45272d4cd5a841ec5fbe807
 
 module.exports = router;
